09c78853261de7e3e59c4f3644f3ed165f0964a0
Maksym Storozhenko <mstoro@softserveinc.com>
2021-02-23 17:53:34 +0200

    deleted jsonify_message func

--- api/utils.py
+++ api/utils.py
@@ -1,13 +1,8 @@
 from io import BytesIO
 
 from PIL import Image
-from flask import jsonify
 
 
 def bytearray_to_img(byte_array, date):
     Image.open(BytesIO(byte_array)).save(f'{date}.jpg')
     return 'saved'
-
-
-def jsonify_message(message):
-    return jsonify({'message': message})
